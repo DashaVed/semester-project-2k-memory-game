5a254b2a134294b68eae874b09d03bf0e3cd9488
Dasha <dasha.ved93@gmail.com>
2022-12-05 22:54:22 +0300

    + window in app

--- window.py
+++ window.py
@@ -1,9 +1,10 @@
-import socket
-import threading
-
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from random import shuffle
 
 import main_window
+
+card_list = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I']
+shuffle(card_list)
 
 
 # пишем своё MainWindow, основанное на Ui_MainWindow (которое мы ранее сгенерировали)
@@ -13,9 +14,31 @@ class Game(QMainWindow, main_window.Ui_MainWindow):
         super(Game, self).__init__()
         self.setupUi(self)
 
+        self.button_1.clicked.connect(lambda: self.clicker(self.button_1, card_list[0]))
+        self.button_2.clicked.connect(lambda: self.clicker(self.button_2, card_list[1]))
+        self.button_3.clicked.connect(lambda: self.clicker(self.button_3, card_list[2]))
+        self.button_4.clicked.connect(lambda: self.clicker(self.button_4, card_list[3]))
+        self.button_5.clicked.connect(lambda: self.clicker(self.button_5, card_list[4]))
+        self.button_6.clicked.connect(lambda: self.clicker(self.button_6, card_list[5]))
+        self.button_7.clicked.connect(lambda: self.clicker(self.button_7, card_list[6]))
+        self.button_8.clicked.connect(lambda: self.clicker(self.button_8, card_list[7]))
+        self.button_9.clicked.connect(lambda: self.clicker(self.button_9, card_list[8]))
+        self.button_10.clicked.connect(lambda: self.clicker(self.button_10, card_list[9]))
+        self.button_11.clicked.connect(lambda: self.clicker(self.button_11, card_list[10]))
+        self.button_12.clicked.connect(lambda: self.clicker(self.button_12, card_list[11]))
+        self.button_13.clicked.connect(lambda: self.clicker(self.button_13, card_list[12]))
+        self.button_14.clicked.connect(lambda: self.clicker(self.button_14, card_list[13]))
+        self.button_15.clicked.connect(lambda: self.clicker(self.button_15, card_list[14]))
+        self.button_16.clicked.connect(lambda: self.clicker(self.button_16, card_list[15]))
+        self.button_17.clicked.connect(lambda: self.clicker(self.button_17, card_list[16]))
+        self.button_18.clicked.connect(lambda: self.clicker(self.button_18, card_list[17]))
+
+    def clicker(self, b, card):
+        b.setText(card)
+        b.setEnabled(False)
+
 
 if __name__ == "__main__":
-    # при запуске клиента мы создаем инстанс приложения, созданного нами главного окна, и все запускаем
     app = QApplication([])
     window = Game()
     window.show()
